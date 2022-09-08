43f8e6bfbc222ac64a765af00cefd676e9a1ce64
Mohammed Ismail <mohammedismail97420@gmail.com>
2022-09-08 13:48:44 +0530

    Update store.config.js

--- config/store.config.js
+++ config/store.config.js
@@ -1,34 +1,58 @@
 export const storeConfigs = {
-    "xtwostore.com": {
-        code: "xtwostore_com",
-        language: "en",
-        html: {
-            head: {
-                title: {
-                    prefix: "",
-                    suffix: ""
-                }
-            }
+  "xtwostore.com": {
+    code: "xtwostore_com",
+    language: "en",
+    html: {
+      head: {
+        title: {
+          prefix: "",
+          suffix: "",
         },
-        system: {
-            configUpdate: {
-                store_config: 3600,
-            },
-        },
-        timestamp: {
-            store_config: {
-                first: '',
-                last: '',
-            }
-        },
-        store_config: {}
+      },
     },
-    "xtwostore.de": {
-        code: "xtwostore_de",
-        language: "de",
+    system: {
+      configUpdate: {
+        store_config: 3600,
+      },
     },
+    timestamp: {
+      store_config: {
+        first: "",
+        last: "",
+      },
+    },
+    store_config: {},
+  },
+  "beta.xtwodev.store": {
+    code: "xtwostore_com",
+    language: "en",
+    html: {
+      head: {
+        title: {
+          prefix: "",
+          suffix: "",
+        },
+      },
+    },
+    system: {
+      configUpdate: {
+        store_config: 3600,
+      },
+    },
+    timestamp: {
+      store_config: {
+        first: "",
+        last: "",
+      },
+    },
+    store_config: {},
+  },
+  "xtwostore.de": {
+    code: "xtwostore_de",
+    language: "de",
+  },
 };
 export const aliasConfigs = {
-    "localhost": "xtwostore.com",
-    "de.xtwodev.store": "xtwostore.de",
+  localhost: "xtwostore.com",
+  "de.xtwodev.store": "xtwostore.de",
 };
